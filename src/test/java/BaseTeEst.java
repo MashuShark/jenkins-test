c5008f0410a42fced7c5ee87b03d683c5e60c14c
MashuShark <us1gfw.maryn@agmail.com>
2022-06-27 12:28:15 +0300

    Add testNg dependency; add BaseTest

---
+++ src/test/java/BaseTeEst.java
@@ -0,0 +1,9 @@
+import org.testng.annotations.Test;
+
+public class BaseTeEst {
+
+    @Test
+    public void test(){
+        System.out.println("I run first test by integration with maven");
+    }
+}
